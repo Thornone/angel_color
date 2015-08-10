abaf16cd72002452c99f6a7fa8c6b3d1661407e2
Juan Manuel Ahumada Vazquez <juanm._94@hotmail.com>
2015-08-10 16:58:34 -0500

    cambios

--- admin.php
+++ admin.php
@@ -97,12 +97,20 @@ $errorMsg = "";
 ?>
 <!DOCTYPE html>
 <html lang="en" class="app">
-<head> <meta charset="utf-8" />
+<head> 
+<meta charset="utf-8" />
 <title>Angel Color</title>
 <meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <link rel="stylesheet" href="css/app.v1.css" type="text/css" />
+ <!-- Plugin CSS  -->
+ <link rel="stylesheet" type="text/css" href="css/fullcalendar.min.css" media="screen">
+ <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+   <!-- Admin Forms CSS -->
+  <link rel="stylesheet" type="text/css" href="css/admin-forms.css">
+
 <script src="js/app.v1.js"></script>
+
 <!--[if lt IE 9]>
 <script src="js/ie/html5shiv.js"></script>
 <script src="js/ie/respond.min.js"></script>
@@ -190,6 +198,168 @@ $errorMsg = "";
 		</section>
 	</section>
 
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery-ui-monthpicker.min.js"></script>
+	<script src="js/jquery-ui-datepicker.min.js"></script>
+	<script src="js/jquery.magnific-popup.js"></script>
+	<script src="js/moment.min.js"></script>
+	<script src="js/fullcalendar.min.js"></script>
+	<script src="js/utility.js"></script>
+	<script src="js/demo.js"></script>
+	<script src="js/main.js"></script>
+	<script type="text/javascript">
+  jQuery(document).ready(function() {
+
+    "use strict";
+
+    // Init Theme Core    
+    Core.init();
+
+    // Init Demo JS  
+    Demo.init();
+
+
+    // Init FullCalendar external events
+    $('#external-events .fc-event').each(function() {
+      // store data so the calendar knows to render an event upon drop
+      $(this).data('event', {
+        title: $.trim($(this).text()), // use the element's text as the event title
+        stick: true, // maintain when user navigates (see docs on the renderEvent method)
+        className: 'fc-event-' + $(this).attr('data-event') // add a contextual class name from data attr
+      });
+
+      // make the event draggable using jQuery UI
+      $(this).draggable({
+        zIndex: 999,
+        revert: true, // will cause the event to go back to its
+        revertDuration: 0 //  original position after the drag
+      });
+
+    });
+
+    var Calendar = $('#calendar');
+    var Picker = $('.inline-mp');
+
+    // Init FullCalendar Plugin
+    Calendar.fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      editable: true,
+      events: [{
+          title: 'Sony Meeting',
+          start: '2015-06-4',
+          end: '2015-06-6',
+          className: 'fc-event-success',
+        }, {
+          title: 'Conference',
+          start: '2015-06-15',
+          end: '2015-06-17',
+          className: 'fc-event-warning'
+        }, {
+          title: 'System Testing',
+          start: '2015-06-26',
+          end: '2015-06-28',
+          className: 'fc-event-primary'
+        },
+      ],
+      viewRender: function(view) {
+        // If monthpicker has been init update its date on change
+        if (Picker.hasClass('hasMonthpicker')) {
+          var selectedDate = Calendar.fullCalendar('getDate');
+          var formatted = moment(selectedDate, 'MM-DD-YYYY').format('MM/YY');
+          Picker.monthpicker("setDate", formatted);
+        }
+        // Update mini calendar title
+        var titleContainer = $('.fc-title-clone');
+        if (!titleContainer.length) {
+          return;
+        }
+        titleContainer.html(view.title);
+      },
+      droppable: true, // this allows things to be dropped onto the calendar
+      drop: function() {
+        // is the "remove after drop" checkbox checked?
+        if (!$(this).hasClass('event-recurring')) {
+          $(this).remove();
+        }
+      },
+      eventRender: function(event, element) {
+        // create event tooltip using bootstrap tooltips
+        $(element).attr("data-original-title", event.title);
+        $(element).tooltip({
+          container: 'body',
+          delay: {
+            "show": 100,
+            "hide": 200
+          }
+        });
+        // create a tooltip auto close timer  
+        $(element).on('show.bs.tooltip', function() {
+          var autoClose = setTimeout(function() {
+            $('.tooltip').fadeOut();
+          }, 3500);
+        });
+      }
+    });
+
+    // Init MonthPicker Plugin and Link to Calendar
+    Picker.monthpicker({
+      prevText: '<i class="fa fa-chevron-left"></i>',
+      nextText: '<i class="fa fa-chevron-right"></i>',
+      showButtonPanel: false,
+      onSelect: function(selectedDate) {
+        var formatted = moment(selectedDate, 'MM/YYYY').format('MM/DD/YYYY');
+        Calendar.fullCalendar('gotoDate', formatted)
+      }
+    });
+
+
+    // Init Calendar Modal via "inline" Magnific Popup
+    $('#compose-event-btn').magnificPopup({
+      removalDelay: 500, //delay removal by X to allow out-animation
+      callbacks: {
+        beforeOpen: function(e) {
+          // we add a class to body indication overlay is active
+          // We can use this to alter any elements such as form popups
+          // that need a higher z-index to properly display in overlays
+          $('body').addClass('mfp-bg-open');
+          this.st.mainClass = this.st.el.attr('data-effect');
+        },
+        afterClose: function(e) {
+          $('body').removeClass('mfp-bg-open');
+        }
+      },
+      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
+
+    // Calendar Modal Date picker
+    $("#eventDate").datepicker({
+      numberOfMonths: 1,
+      prevText: '<i class="fa fa-chevron-left"></i>',
+      nextText: '<i class="fa fa-chevron-right"></i>',
+      showButtonPanel: false,
+      beforeShow: function(input, inst) {
+        var newclass = 'admin-form';
+        var themeClass = $(this).parents('.admin-form').attr('class');
+        var smartpikr = inst.dpDiv.parent();
+        if (!smartpikr.hasClass(themeClass)) {
+          inst.dpDiv.wrap('<div class="' + themeClass + '"></div>');
+        }
+      }
+
+    });
+
+
+  });
+  </script>
+
 	<script src="js/app.plugin.js"></script>
+
+	<div id="ui-monthpicker-div" class="ui-datepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all"></div>
+	<div id="ui-datepicker-div" class="ui-datepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all"></div>
 </body>
 </html>
